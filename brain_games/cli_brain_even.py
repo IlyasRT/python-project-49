654dbd5a6a3749ada57a1a4cdecb921a28dfaabe
ilyas-r <riggit5->
2023-09-18 15:55:01 +0300

    STEP 5 - BRAIN GAMES

--- brain_games/cli_brain_even.py
+++ brain_games/cli_brain_even.py
@@ -1,24 +1,38 @@
 from random import randint
-from prompt import string
+import prompt 
+#from brain_games.cli import welcome_user
+from brain_games.cli import *
+#import brain_games.cli
 
 
+'''
+import brain_games.cli
 def main():
-	name='Ilyas'
-	print(f'Answer "yes" if the number is even, otherwise answer "no".')
-	count = 0
-	correction_of_user_answer = True
-	while count < 3:
-	    random_number = randint(1, 10)
-	    print(f'Question: {random_number}')
-	    user_answer = prompt.string('Your answer:').lower()
-	    if (random_number % 2 == 0 and user_answer == 'yes'.lower()) or (random_number % 2 != 0 and user_answer == 'no'.lower()):
-		count += 1
-		print(f'Correct!')
+    
+'''
 
-	    else:
-		count = 3
-		correction_of_user_answer = False
-		print(f'\'yes\' is wrong answer ;\(. Correct answer was \'no\'. Let\'s try again, {name}!')
-	if correction_of_user_answer==True:
-	    print(f'Congratulations, {name}! {count} - {correction_of_user_answer}')
+print('******brain_even*********')
+#print(name)
+#print(name,welcome_user)
+def brain_even():
+    #brain_games.cli.welcome_user()
+    name='Ilyas' #использовал в pycharm
+    print(f'Answer "yes" if the number is even, otherwise answer "no".')
+    count = 0
+    correction_of_user_answer = True
+    while count < 3:
+        random_number = randint(1, 10)
+        print(f'Question: {random_number}')
+        user_answer = prompt.string('Your answer:').lower()
+        if (random_number % 2 == 0 and user_answer == 'yes'.lower()) or (
+                random_number % 2 != 0 and user_answer == 'no'.lower()):
+            count += 1
+            print(f'Correct!')
+
+        else:
+            count = 3
+            correction_of_user_answer = False
+            print(f'\'yes\' is wrong answer ;\(. Correct answer was \'no\'. Let\'s try again, {name}!')
+    if correction_of_user_answer == True:
+        print(f'Congratulations, {name}!')
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,5 +2,11 @@
 
 from brain_games.cli import welcome_user
 
-main
-welcome_user
+def main():
+    welcome_user()
+    
+if __name__ == '__main__':
+    main()
+    
+
+
