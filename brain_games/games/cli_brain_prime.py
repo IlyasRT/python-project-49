61b23194470d0787d502cc6b57b6ba1862864b7a
ilyas-r <riggit5->
2023-09-22 17:55:20 +0300

    add

--- brain_games/games/cli_brain_prime.py
+++ brain_games/games/cli_brain_prime.py
@@ -3,36 +3,33 @@ import prompt
 from brain_games.games.cli import welcome_user
 from brain_games.games.constants import NUMBER_OF_REPETITIONS
 print('brain-prime')
-
+   
 
 def brain_prime():
     name = welcome_user()
     count = 0
-    correction_of_user_answer = True
+    correction = True
     print('Answer \"yes\" if given number is prime. Otherwise answer \"no\".')
     while count < NUMBER_OF_REPETITIONS:
-        number = randint(1, 100)
+        number = randint(1, 30)
         print(f'Question: {number}')
         d = []
         for i in range(1, number + 1):
             if number % i == 0:
                 d.append(i)
         an = prompt.string('Your answer: ').lower()
-        if an == 'yes' and len(d) == 2:
-            count += 1
-            print('Correct!')
-        elif an == 'no' and len(d) > 2:
+        if (an == 'yes' and len(d) == 2) or (an == 'no' and len(d) > 2):
             count += 1
             print('Correct!')
         elif an == 'yes' and len(d) > 2:
-            correction_of_user_answer = False
+            correction = False
             count += 3
             print(f'\'{an}\' is wrong answer;(. Correct answer was \'no\'.')
             print(f'Let\'s try again, {name}!')
-        elif an == 'no' and len(d) == 2:
-            correction_of_user_answer = False
+        else:
+            correction = False
             count += 3
             print(f'\'{an}\' is wrong answer;(. Correct answer was \'yes\'.')
             print(f'Let\'s try again, {name}!')
-    if correction_of_user_answer is True:
+    if correction is True:
         print(f'Congratulations, {name}!')
--- brain_games/games/constants.py
+++ brain_games/games/constants.py
@@ -1,1 +1,3 @@
 NUMBER_OF_REPETITIONS = 3
+ANSWER_FOR_USER_1 = ' is wrong answer;(. Correct answer was '
+ANSWER_FOR_USER_2 = 'Let\'s try again, '
