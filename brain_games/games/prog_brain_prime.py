94d3a991dac297a479292abba741a0f391498f04
ilyas-r <riggit5->
2023-09-24 21:44:06 +0300

    add prog_brain_prime.py + brain_prime.py

--- brain_games/games/prog_brain_prime.py
+++ brain_games/games/prog_brain_prime.py
@@ -1,41 +1,19 @@
 from random import randint
-import prompt
-from brain_games.games.cli import welcome_user
-from brain_games.games.constants import NUMBER_OF_REPETITIONS, A_YES, A_NO, A_CM
-
-print('brain-prime')
+print('brain-progression')
+DESCRIPTION_OF_GAME = 'Answer \"yes\" if given number is prime. Otherwise answer \"no\".'
 
 
-def brain_prime():
-    name = welcome_user()
-    count = 0
-    correction = True
-    print('Answer \"yes\" if given number is prime. Otherwise answer \"no\".')
-    while count < NUMBER_OF_REPETITIONS and correction is True:
-        number = randint(1, 30)
-        print(f'Question: {number}')
-        d = []
-        for i in range(1, number + 1):
-            if number % i == 0:
-                d.append(i)
-        an = prompt.string('Your answer: ')
-        if len(d) == 2:
-            var = 2
-        else:
-            var = 3
-        ans = (an, var)
-        match ans:
-            case 'yes', 2:
-                count += 1
-                print('Correct!')
-            case 'no', 3:
-                count += 1
-                print('Correct!')
-            case 'no', 2:
-                correction = False
-                print(f'{A_NO}\n{A_CM}{name}!')
-            case 'yes', 3:
-                correction = False
-                print(f'{A_YES}\n{A_CM}{name}!')
-    if correction is True:
-        print(f'Congratulations, {name}!')
+def initial_data():
+    number = randint(2, 30)
+    print(f'Question: {number}')
+    d = []
+    for i in range(1, number + 1):
+        if number % i == 0:
+            d.append(i)
+
+    if len(d) == 2:
+        correct_answer = 'yes'
+        return correct_answer
+    else:
+        correct_answer = 'no'
+        return correct_answer
