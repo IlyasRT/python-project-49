8e157c96ffaa3ea1d5661093a20fc16439b4125b
ilyas-r <riggit5->
2023-09-24 23:54:12 +0300

    add after correction constant.py

--- brain_games/games/constants.py
+++ brain_games/games/constants.py
@@ -1,4 +1,1 @@
 NUMBER_OF_REPETITIONS = 3
-A_YES = '\'yes\' is wrong answer;(. Correct answer was \'no\'.'
-A_CM = 'Let\'s try again, '
-A_NO = '\'no\' is wrong answer;(. Correct answer was \'yes\'.'
