05c2b4293a14e4a7aa923477ddfde5082d574e2b
ilyas-r <riggit5->
2023-10-04 09:41:30 +0300

    add files

--- brain_games/games/prog_brain_progression.py
+++ brain_games/games/prog_brain_progression.py
@@ -1,27 +1,37 @@
 from random import randint
 print('brain-progression')
-DESCR = 'What number is missing in the progression?'
+DESCR = 'What number is missing in the prog?'
 
 
 def initial_data():
-    def check_progression():
-        length_of_progression = randint(5, 10)
-        position_of_unknown_element = randint(0, length_of_progression - 1)
-        delta_of_progression = randint(1, 5)
-        x = (length_of_progression * delta_of_progression) + 1
-        progression = []
-        for i in range(delta_of_progression, x, delta_of_progression):
-            progression.append(i)
-        ue = progression[position_of_unknown_element]
-        progression[position_of_unknown_element] = '..'
-        return progression, ue
-    progression, ue = check_progression()
-
-    def build_progression(x):
-        prog = ''
-        for el in x:
-            prog = prog + ' ' + str(el)
-        return prog[1:]
-    expression = build_progression(progression)
-    answ = str(ue)
+    #prog, ue = calc_prog()
+    first, last, delta, index = calc_prog()
+    progres, unk_el = build_prog (first, last, delta, index)
+    expression = build_question(progres)
+    answ = str(unk_el)
     return expression, answ
+
+
+def calc_prog():
+    len_prog = randint(5, 10)
+    index_unk_el = randint(0, len_prog - 1)
+    delta_prog = randint(1, 5)
+    first_prog_el = delta_prog
+    last_prog_el = (len_prog * delta_prog) + 1
+    return first_prog_el, last_prog_el, delta_prog, index_unk_el
+
+
+def build_prog(first_prog_el,last_prog_el,delta_prog,index_unk):   
+    prog = []
+    for i in range(first_prog_el, last_prog_el, delta_prog):
+        prog.append(i)
+    ue = prog[index_unk]
+    prog[index_unk] = '..'
+    return prog, ue
+
+
+def build_question(progres):
+    prog_str = list(map(lambda x: str(x),progres))
+    prog_for_print = " ".join(prog_str)
+    return prog_for_print
+
