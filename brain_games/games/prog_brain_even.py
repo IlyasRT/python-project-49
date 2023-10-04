ab485abfae41d0c59688a2b5f373bcefda516378
ilyas-r <riggit5->
2023-10-04 07:54:25 +0300

    add files

--- brain_games/games/prog_brain_even.py
+++ brain_games/games/prog_brain_even.py
@@ -5,15 +5,16 @@ DESCR = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def initial_data():
     expression = randint(1, 10)
-
-    def check_even(x):
-        if int(x % 2) == 0:
-            answer = True
-        elif int(x % 2) != 0:
-            answer = False
-        return answer
     if check_even(expression) is True:
         answ = 'yes'
     else:
         answ = 'no'
     return expression, answ
+
+def check_even(x):
+    if int(x % 2) == 0:
+        answer = True
+    elif int(x % 2) != 0:
+        answer = False
+    return answer
+
--- brain_games/games/prog_brain_gcd.py
+++ brain_games/games/prog_brain_gcd.py
@@ -5,16 +5,17 @@ DESCR = 'Find the greatest common divisor of given numbers.'
 
 def initial_data():
     a, b = randint(1, 10), randint(1, 10)
-
-    def check_gcd(x, y):
-        list_of_number = [x, y]
-        list_of_dividers = []
-        min_number = min(list_of_number)
-        for i in range(1, min_number + 1):
-            if x % i == 0 and y % i == 0:
-                list_of_dividers.append(i)
-        answer = max(list_of_dividers)
-        return answer
     expression = f'{a} {b}'
     answ = str(check_gcd(a, b))
     return expression, answ
+
+def check_gcd(x, y):
+    list_of_number = [x, y]
+    list_of_dividers = []
+    min_number = min(list_of_number)
+    for i in range(1, min_number + 1):
+        if x % i == 0 and y % i == 0:
+            list_of_dividers.append(i)
+    answer = max(list_of_dividers)
+    return answer
+
